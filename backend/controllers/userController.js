7bcd06e34b716089725e48309d92f4da038f9bf4
Nithin Prathapan <nithinprathapan369@gmail.com>
2024-05-27 20:58:07 +0530

    user routers initialization

---
+++ backend/controllers/userController.js
@@ -0,0 +1,5 @@
+export const getAllUsers = () => {};
+export const createUser = () => {};
+export const deleteUser = () => {};
+export const getUser = () => {};
+export const updateUser = () => {};
--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
-import productRoutes from './routes/productRoutes.js'
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
 
 app.use("/api/product", productRoutes);
+app.use("/api/user", userRoutes);
 
 const port = process.env.PORT || 9000;
 app.listen(port, () => {
